6d6785d5fd3066443b4bcb4595eb63c8bac83674
ColinGundach <colin.gundlach@hotmail.de>
2017-03-28 11:44:43 +0200

    Add files via upload

---
+++ EingabeBegrüßung.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+<title>Test</title>
+
+<h1>Wie spät ist es</h1>
+
+<form action="ausgabetest1.php" method="get">
+    <p>Gib deinen Namen ein: <input type="text" name="benutzername" /></p>
+    <input type="submit" value="absenden" />
+</form>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
